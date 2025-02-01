d2224be0580bdbe7f51f1464cc41104ff443175e
Henrique Hadermeck <henriquezanferrari@gmail.com>
2025-02-01 10:28:14 +0100

    feat: enhance space details screen with current and invited members widgets

--- packages/app/lib/features/space/presentation/space_details_screen.dart
+++ packages/app/lib/features/space/presentation/space_details_screen.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ob/core/di/di.dart';
+import 'package:ob/features/registration/convert_to_permanent_widget.dart';
+import 'package:ob/features/space/domain/space_model.dart';
 import 'package:ob/features/space/presentation/bloc/space_bloc.dart';
 import 'package:ob/ui/widgets/widgets.dart';
 
@@ -56,59 +59,107 @@ class _View extends StatelessWidget {
                   subtitle: Text(space.description ?? 'No description'),
                 ),
                 // users
-                ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: space.users.length,
-                  itemBuilder: (context, index) {
-                    final user = space.users[index];
-                    return ListTile(
-                      contentPadding: EdgeInsets.zero,
-                      title: Text(user.name),
-                    );
-                  },
-                ),
-                const SizedBox(height: 20),
-                // invitations
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Invited users',
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        showOBModalBottomSheet<String>(
-                          context: context,
-                          child: const _InviteUserModal(),
-                        ).then((email) {
-                          if (email != null) {
-                            context.read<SpaceBloc>().add(InviteUser(email));
-                          }
-                        });
-                      },
-                      child: const Text('Add member'),
-                    ),
-                  ],
-                ),
-                ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: space.invitedEmails?.length ?? 0,
-                  itemBuilder: (context, index) {
-                    final email = space.invitedEmails![index];
-                    return ListTile(
-                      contentPadding: EdgeInsets.zero,
-                      title: Text(email),
-                    );
-                  },
-                ),
+                const SizedBox(height: 16),
+                _CurrentMembersWidget(space: space),
+                const SizedBox(height: 16),
+                _InvitedMembersWidget(space: space),
               ],
             ),
           );
         },
       ),
+    );
+  }
+}
+
+class _CurrentMembersWidget extends StatelessWidget {
+  const _CurrentMembersWidget({
+    required this.space,
+  });
+
+  final SpaceModel space;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Current members',
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        ListView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          padding: EdgeInsets.zero,
+          itemCount: space.users.length,
+          itemBuilder: (context, index) {
+            final user = space.users[index];
+            final text = user.name.isEmpty ? user.userId : user.name;
+            return ListTile(
+              leading: const Icon(Icons.person),
+              contentPadding: EdgeInsets.zero,
+              title: Text(text),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class _InvitedMembersWidget extends StatelessWidget {
+  const _InvitedMembersWidget({
+    required this.space,
+  });
+
+  final SpaceModel space;
+
+  @override
+  Widget build(BuildContext context) {
+    final isAnonymous = FirebaseAuth.instance.currentUser?.isAnonymous ?? true;
+
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Invited users',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            if (!isAnonymous)
+              TextButton(
+                onPressed: () {
+                  showOBModalBottomSheet<String>(
+                    context: context,
+                    child: const _InviteUserModal(),
+                  ).then((email) {
+                    if (email != null) {
+                      context.read<SpaceBloc>().add(InviteUser(email));
+                    }
+                  });
+                },
+                child: const Text('Add member'),
+              ),
+          ],
+        ),
+        if (isAnonymous)
+          const ConvertToPermanentWidget()
+        else
+          ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: space.invitedEmails?.length ?? 0,
+            itemBuilder: (context, index) {
+              final email = space.invitedEmails![index];
+              return ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: Text(email),
+              );
+            },
+          ),
+      ],
     );
   }
 }
